b8ad99ea791ba403d1154da5a6f2f1648bd6f65e
Miguel <miguelriostangarife@gmail.com>
2023-02-26 18:13:28 -0500

    Update thermostat

--- Python exercises/Thermostat/Thermostat1759C.py
+++ Python exercises/Thermostat/Thermostat1759C.py
@@ -3,22 +3,24 @@ Problem - 1759C - Codeforces
 Code made by Miguel Rios Tangarife
 In this problem we have an old thermostat and we need to find the minimum number of operations required 
 to get temperature b from temperature a, or say that it is impossible
-In the exercise we are given the variables a,b -->(initial temperature, final temperature)
-and l,r,x  where l,r are the range of temperature and 
+
+In the exercise we are given the variables:
+L,r,x  where L,r are the range of temperature and 
 x is the minimum temperature change 
+a,b -->(initial temperature, final temperature)
 """
 output=[] #List containing the output values ​​that are saved with each for iteration
 t=int(input()) # t --> number of test cases
 for i in range(t):
-    l, r, x = map(int, input().split()) #Allows inputs to be entered like this: 3 5 6
+    L, r, x = map(int, input().split()) #Allows inputs to be entered like this: 3 5 6
     a, b = map(int, input().split()) 
-    if a == b: #When this happen the thermostat is already set up correctly
+    if a == b: #Thermostat is already set up correctly
         output.append(0)# Add the number of operations required in the output list 
     elif abs(a-b) >= x:
         output.append(1) 
-    elif r - max(a, b) >= x or min(a, b) - l >= x:
+    elif r - max(a, b) >= x or min(a, b) - L >= x:
         output.append(2)
-    elif r - b >= x and a - l >= x or r - a >= x and b - l >= x:
+    elif r - b >= x and a - L >= x or r - a >= x and b - L >= x:
         output.append(3)
     else:
         output.append(-1)
--- Python exercises/Thermostat/ThermostatEXTENDED.py
+++ Python exercises/Thermostat/ThermostatEXTENDED.py
@@ -1,19 +1,20 @@
-#NUMERO DE CASOS DE PRUEBA
+#Number of test cases
 while True:
     try:
-        t = int(input("Ingrese un número entero único en el rango de 1 a 10^4: "))
-        if t < 1 or t > 10**4:
-            print("El número debe estar dentro del rango especificado.")
+        t = int(input())
+        if t <= 1 or t >= 10**4: #((1≤t≤10^4)) 
+            print("The number must be within the specified range.")
         else:
             break
     except ValueError:
-        print("Por favor, ingrese un número entero.")
-output=[]
+        print("Please enter an integer number")
+
+output=[] #List containing the output values ​​that are saved with each for iteration
 for i in range(t):
-    l, r, x = map(int, input().split())
-    a, b = map(int, input().split())
-    if a == b:
-        output.append(0)
+    l, r, x = map(int, input().split()) #Allows inputs to be entered like this: 3 5 6
+    a, b = map(int, input().split()) 
+    if a == b: #When this happen the thermostat is already set up correctly
+        output.append(0)# Add the number of operations required in the output list 
     elif abs(a-b) >= x:
         output.append(1) 
     elif r - max(a, b) >= x or min(a, b) - l >= x:
@@ -22,5 +23,5 @@ for i in range(t):
         output.append(3)
     else:
         output.append(-1)
-for elem in output: 
+for elem in output: #Display the data in an ordered way
     print(elem)
