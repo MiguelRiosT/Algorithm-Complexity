763c4b4d7b124065da66fac4a6d0e0afd851b645
Miguel <miguelriostangarife@gmail.com>
2023-02-26 17:27:22 -0500

    Update Thermostat

--- Python exercises/Thermostat/Thermostat1759C.py
+++ Python exercises/Thermostat/Thermostat1759C.py
@@ -1,34 +1,18 @@
-"""
-def solve():
-    l, r, x = map(int, input().split())
-    a, b = map(int, input().split())
-    if a == b:
-        return 0
-    if abs(a - b) >= x:
-        return 1
-    if r - max(a, b) >= x or min(a, b) - l >= x:
-        return 2
-    if r - b >= x and a - l >= x or r - a >= x and b - l >= x:
-        return 3
-    return -1
-t = int(input())
-for _ in range(t):
-    print(solve())
-"""
-resultados=[]
+
+output=[]
 t=int(input())
 for i in range(t):
     l, r, x = map(int, input().split())
     a, b = map(int, input().split())
-    if a == b:#0
-        resultados.append(0)
-    elif abs(a-b) >= x:#1
-        resultados.append(1) 
-    elif r - max(a, b) >= x or min(a, b) - l >= x:#2
-        resultados.append(2)
-    elif r - b >= x and a - l >= x or r - a >= x and b - l >= x:#3
-        resultados.append(3)
+    if a == b:
+        output.append(0)
+    elif abs(a-b) >= x:
+        output.append(1) 
+    elif r - max(a, b) >= x or min(a, b) - l >= x:
+        output.append(2)
+    elif r - b >= x and a - l >= x or r - a >= x and b - l >= x:
+        output.append(3)
     else:
-        resultados.append(-1)
-for elem in resultados: 
+        output.append(-1)
+for elem in output: 
     print(elem)
--- Python exercises/Thermostat/ThermostatEXTENDED.py
+++ Python exercises/Thermostat/ThermostatEXTENDED.py
@@ -8,14 +8,19 @@ while True:
             break
     except ValueError:
         print("Por favor, ingrese un número entero.")
-
-#PRIMERA Y SEGUNDA LINEA DE INPUT
-l, r, x = map(int, input("Ingrese los valores de la primera línea separados por espacios: ").split())
-# Pedimos al usuario que ingrese los valores de la segunda línea
-a, b = map(int, input("Ingrese los valores de la segunda línea separados por espacios: ").split())
-# Imprimimos los valores para verificar que se hayan leído correctamente
-print("l =", l)
-print("r =", r)
-print("x =", x)
-print("a =", a)
-print("b =", b)
+output=[]
+for i in range(t):
+    l, r, x = map(int, input().split())
+    a, b = map(int, input().split())
+    if a == b:
+        output.append(0)
+    elif abs(a-b) >= x:
+        output.append(1) 
+    elif r - max(a, b) >= x or min(a, b) - l >= x:
+        output.append(2)
+    elif r - b >= x and a - l >= x or r - a >= x and b - l >= x:
+        output.append(3)
+    else:
+        output.append(-1)
+for elem in output: 
+    print(elem)
